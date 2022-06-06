90803a2c556c91f9c5de9fd056cc071449e73780
Adam Del Castillo-Call <adam.delcastillocall6882@stu.mtec.edu>
2022-06-06 11:35:48 -0600

    Second commit, I hav fixed the initial issue but now I have a new issue in the underscores for hangman.

--- hangMan.js
+++ hangMan.js
@@ -6,15 +6,41 @@ console.log(ranNum);
 let finalWord = posWords[ranNum];
 console.log(finalWord);
 
+function guessWord(){
+    let guess = prompt("What is your first guess? ");
+    console.log(guess);
+    return guess;
+}
 
-// let guess = prompt("What is your first guess? ");
-// console.log(guess);
+document.body.style.backgroundColor = "#1a001a";
+document.body.style.fontFamily = 'Plus Jakarta Sans', 'sans-serif';
+document.body.style.color = "white";
+
+guess = guessWord();
+
+finalWordNum = finalWord.length;
+console.log(finalWordNum);
+
+function findUnder() {
+    var dashes = [];
+    for (let i = 0; i < finalWordNum; i++) {
+        dashes.push("_ ");
+    }
+    return dashes;
+}
+
+dashes = findUnder();
+
+// function dashOut(guess, finalWord) {
+//     if () {
+
+//     }
+//     else {
+
+//     }
+//     return gameboardText;
+// }
 
 
 let gameboard = document.getElementById('gameboard');
-gameboard.style.color = 'red';
-
-
-function guessWord(){
-    let guess = prompt("")
-}
+gameboard.innerHTML = "Gameboard: " + dashes;
