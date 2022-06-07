2f66e40f515abcdfc62a4797f3e51a5aabe7625c
Adam Del Castillo-Call <adam.delcastillocall6882@stu.mtec.edu>
2022-06-07 01:47:18 -0600

    Third commit, the program is almost done, I just need to get it to load after every prompt then give an alert when you've ran out of attempts.

--- hangMan.js
+++ hangMan.js
@@ -1,46 +1,86 @@
 let posWords = ['cat', 'dog', 'frog', 'yes'];
+let triesNum = 5;
 
 ranNum = Math.floor(Math.random() * 4);
 console.log(ranNum);
 
+
 let finalWord = posWords[ranNum];
 console.log(finalWord);
 
-function guessWord(){
-    let guess = prompt("What is your first guess? ");
-    console.log(guess);
-    return guess;
-}
+
+let finalWordArray = finalWord.split("");
+console.log(finalWordArray);
+
 
 document.body.style.backgroundColor = "#1a001a";
 document.body.style.fontFamily = 'Plus Jakarta Sans', 'sans-serif';
 document.body.style.color = "white";
 
-guess = guessWord();
 
-finalWordNum = finalWord.length;
-console.log(finalWordNum);
+while (triesNum > 0) {
+    function guessWord(){
+        let guess = prompt("What is your guess? ");
+        while (guess === "") {
+            guess = prompt("You need to type something in. What is your guess? ");
+        }
+        console.log(guess);
+        return guess;
+    }
 
-function findUnder() {
+    guess = guessWord();
+
+    let guessArray = guess.split("");
+    console.log(guessArray);
+    guessNum = guess.length;
+
+    finalWordNum = finalWord.length;
+    console.log(finalWordNum);
+
+
+
     var dashes = [];
     for (let i = 0; i < finalWordNum; i++) {
-        dashes.push("_ ");
+            dashes.push("_ ");
     }
-    return dashes;
+
+
+    let dupe = [];
+    let y = 0
+    finalWordArray.forEach(function(item) {
+        for (let x = 0; x < guessNum; x++) {
+            if (guessArray[x] === item) {
+                console.log("MATCH");
+                dashes.splice(y, 1);
+                console.log(dashes);
+                dashes.splice(y, 0, item);
+                console.log(dashes);
+                dupe.push(" " + guessArray[x]);
+            }
+            else {
+                console.log("NOT A MATCH");
+                dupe.push(" " + guessArray[x]);
+            }
+        }
+        y++;
+    })
+
+    let wrongGuess = [...new Set(dupe)];
+
+
+
+    dashesWithoutCommas = dashes.join('')
+
+    let gameboard = document.getElementById('gameboard');
+    gameboard.innerHTML = "Gameboard: " + dashesWithoutCommas;
+
+
+    let tries = document.getElementById('triesLeft');
+    tries.innerHTML = "Tries Left: " + triesNum;
+
+
+    let wrongLet = document.getElementById('wrongLetters');
+    wrongLet.innerHTML = "Wrong letters guessed: " + wrongGuess;
+
+    triesNum--;
 }
-
-dashes = findUnder();
-
-// function dashOut(guess, finalWord) {
-//     if () {
-
-//     }
-//     else {
-
-//     }
-//     return gameboardText;
-// }
-
-
-let gameboard = document.getElementById('gameboard');
-gameboard.innerHTML = "Gameboard: " + dashes;
